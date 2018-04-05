c5762314c7938e67f1a1464c7d2b97ae9e73960f
Pranav Nutalapati <pranavnutalapati@gmail.com>
2018-04-05 12:41:22 +0530

    Initial commit, added database schema

---
+++ database.js
@@ -0,0 +1,48 @@
+let config = require('./config.json')
+
+let mongoose = require('mongoose')
+let { Schema } = mongoose
+
+mongoose.connect(config.dbUri)
+
+
+const studentSchema = new Schema({
+	pin: String,
+	grade: Number,
+	section: String,
+	name: String,
+	used: {
+		type: Boolean,
+		default: false
+	},
+	voted: {
+		type: Boolean,
+		default: false
+	},
+	subjects: [ Schema.Types.ObjectId ]
+})
+
+const subjectSchema = new Schema({
+	name: String,
+	grade: Number,
+	teacherId: Schema.Types.ObjectId
+})
+
+const teacherSchema = new Schema({
+	name: String,
+	subjects: [ Schema.Types.ObjectId ]
+})
+
+const submissionSchema = new Schema({
+	subjectId: Schema.Types.ObjectId,
+	studentId: Schema.Types.ObjectId,
+	form: Schema.Types.Mixed
+})
+
+module.exports = {
+	Student: mongoose.model('Student', studentSchema),
+	Subject: mongoose.model('Subject', subjectSchema),
+	Teacher: mongoose.model('Teacher', teacherSchema),
+
+	Submission: mongoose.model('Submission', submissionSchema),
+}
